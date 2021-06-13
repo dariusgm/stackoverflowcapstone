9267c2aff0485b7eb8f1a150a0e67e3deb3a6d55
Darius Murawski <dariusmurawski@hotmail.com>
2021-06-13 20:55:30 +0200

    add  preprocessing by column

--- preprocessing.py
+++ preprocessing.py
@@ -4,12 +4,25 @@ from config import config
 import json
 import csv
 
-
-def convert_to_json_by_column(input_path:str, output_path:str, column:str)-> None:
+def convert_to_json_by_column(input_path:str, output_path:str, column:str, process_cols:dict)-> None:
     '''
     Convert the content to several json files, splitted by the column name provided.
     
     '''
+    with open(output_path, 'wt') as output_file:
+        with open(input_path, 'rt') as csvfile:
+            file = csv.reader(csvfile, delimiter=',') 
+            for line_index, elements in enumerate(file):
+                if line_index == 0:
+                    # we have the header already
+                    continue
+                else:
+                    # process selected column, 
+                    ## for visualisation in jupyter
+                    ## for ML Model
+                    result_line = process_data(elements, process_cols, column)
+
+                output_file.write(json.dumps(result_line) + '\n')
 
 def convert_to_json(input_path: str, output_path:str, leave_columns) -> None:
     '''
@@ -24,27 +37,31 @@ def convert_to_json(input_path: str, output_path:str, leave_columns) -> None:
                     process_cols = extract_header(elements, leave_columns)
                     continue
                 else:
-                    result_line = process_data(elements, process_cols)
+                    # process all column, for spark
+                    result_line = process_data(elements, process_cols, None)
 
                 output_file.write(json.dumps(result_line) + '\n')
-                        
 
-def process_data(elements:list, process_cols:dict) -> dict:
+    return process_cols
+               
+
+def process_data(elements:list, process_cols:dict, column: str) -> dict:
     result_line = {}
     for column_index, e in enumerate(elements):
         column_meta_data = process_cols[column_index] 
         action = column_meta_data['action'] 
         name = column_meta_data['name']
-        if action == 'explode':
-            key = f"{name}_{e}"
-            result_line[key] = 1
-        elif action == 'leave':
-            key = f"{name}"
-            result_line[key] = 1
+        if column == None or name == column: 
+            if action == 'explode':
+                key = f"{name}_{e}"
+                result_line[key] = 1
+            elif action == 'leave':
+                key = f"{name}"
+                result_line[key] = 1
             
     return result_line
 
-def extract_header(elements:list, leave_cols:list):
+def extract_header(elements:list, leave_cols:list) -> dict:
     process_cols = {}
     for column_index, e in enumerate(elements):
         if e in leave_cols:
@@ -63,10 +80,15 @@ def main():
             data_path = element['data_path']
             json_path = element['json_path']
             leave_columns = element['leave_columns']
-            print(data_path)
-            convert_to_json(input_path=data_path, output_path=json_path, leave_columns=leave_columns)
-
-
+            print(f"IN: {data_path}")
+            cols_dict = convert_to_json(input_path=data_path, output_path=json_path, leave_columns=leave_columns)
+            # process by columns for visualtisation purpose
+            # and for model buliding
+            for column_index, column in cols_dict.items():
+                if column['name'] not in leave_columns:
+                    output_path = os.path.join("cache", f"{year}_{column['name']}.json")
+                    print(f"OUT: {output_path}")
+                    convert_to_json_by_column(data_path, output_path, column, cols_dict)
 
 if __name__ == '__main__':
     main()
