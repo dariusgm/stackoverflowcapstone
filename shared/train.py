2104b8bcc7ddc01a500990ff82a8a3584159447a
Darius Murawski <d.murawski@emetriq.com>
2021-08-03 21:34:56 +0200

    include latest feedback from udacity review

--- shared/train.py
+++ shared/train.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
@@ -6,19 +7,29 @@ from tensorflow.keras import layers
 
 # Make numpy printouts easier to read.
 np.set_printoptions(precision=3, suppress=True)
+epochs = 1
 
-
-def train_on_df(df, full: bool = False):
-    if full:
-        epochs = 3
-    else:
-        epochs = 1
-
-    # only use rows with label data
-    df = df[df['ConvertedComp'] > 0]
+def clean(df):
     target = df['ConvertedComp']
     df = df.drop(columns=['CompTotal', "Respondent", "ConvertedComp"])
+    return df, target
 
+
+def train_splitted(train: pd.DataFrame, test: pd.DataFrame):
+    X_train, y_train = clean(train)
+    X_test, y_test = clean(test)
+    model = build_and_compile_model()
+
+    history = model.fit(X_train.values, y_train.values,
+                        verbose=0,
+                        epochs=3, batch_size=32,
+                        validation_data=(X_test, y_test))
+    return model, history, list(X_train.columns)
+
+
+def train_on_df(df):
+
+    df, target = clean(df)
     X_train, X_test, y_train, y_test = train_test_split(df, target,
                                                         test_size=0.2)
 
@@ -26,7 +37,7 @@ def train_on_df(df, full: bool = False):
 
     history = model.fit(X_train.values, y_train.values,
                         verbose=1,
-                        epochs=epochs, batch_size=1,
+                        epochs=1, batch_size=2,
                         validation_data=(X_test, y_test))
     return model, history, list(df.columns)
 
--- train_all.py
+++ train_all.py
@@ -1,28 +1,68 @@
 import json
+import math
 import os
 import sys
 
-import numpy as np
 import pandas as pd
-from shared.train import train_on_df
-from sklearn.model_selection import train_test_split
+from shared.train import train_splitted
 import tensorflowjs as tfjs
 
 
 def main():
+    folds = 5
     df = pd.read_json(os.path.join("data", "all_2020.json"), dtype=float,
                       lines=True).fillna(0)
-    model, history, columns = train_on_df(df, full=True)
 
-    tfjs.converters.save_keras_model(model, os.path.join("data", "model", "tfjs_2020.model"))
-    print(history.history)
+    # Only train on real salary values
+    df = df[df['ConvertedComp'] > 0]
+    df = df.reset_index()
+
+    size = len(df)
+    chunk_size = int(size / folds+1) # adding 1 here, as we always start with chunk from zero
+    training_set = list(range(0, size, chunk_size))
+
+    models = []
+    histories = []
+    all_columns = []
+
+    print(f"Total Records: {size}")
+
+    for index, lower_bound in enumerate(training_set):
+        higher_bound = min(lower_bound + chunk_size, size)
+        copy_df = df.copy(True)
+        print(f"Training fold {index}")
+        test_df = copy_df.isin(range(lower_bound, higher_bound))
+        training_df = copy_df.drop(range(lower_bound, higher_bound), inplace=False)
+
+
+        model, history, columns = train_splitted(training_df, test_df)
+        print(history.history)
+        models.append(model)
+        histories.append(history)
+        all_columns.append(columns)
+
+    # get winner
+    winner_index = 0
+    winner_score = math.inf
+    for index, history in enumerate(histories):
+        for score in history.history['val_mean_squared_error']:
+            if score < winner_score:
+                winner_index = index
+                winner_score = score
+
+
+    print(f"winning model after kfold: {winner_index}, using for prediction")
+
+
+    tfjs.converters.save_keras_model(models[winner_index], os.path.join("data", "model", "tfjs_2020.model"))
+
     with open(os.path.join("data", "meta", "metrics.json"),
               'wt') as feature_list:
-        feature_list.write(json.dumps(history.history))
+        feature_list.write(json.dumps(histories[winner_index].history))
 
     with open(os.path.join("data", "meta", "feature_list.json"),
               'wt') as feature_list:
-        feature_list.write(json.dumps(columns))
+        feature_list.write(json.dumps(all_columns[winner_index]))
 
 if __name__ == '__main__':
     main()
